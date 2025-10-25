32a0cc1f2e99245c781cd70f25667cdb020e9edf
Juan Felipe Giraldo <juanfegiraldo97@gmail.com>
2025-10-25 03:49:48 +0000

    Enhance output formatting by adding addresses of variables and improving clarity in print statements

--- cmd/tutorial_7/main.go
+++ cmd/tutorial_7/main.go
@@ -6,7 +6,7 @@ func main() {
 	var p *int32 = new(int32)
 	var i int32
 	fmt.Printf("The value of p points to is: %v\nthe address is: %v", *p, p)
-	fmt.Printf("\nThe value if i is: %v", i)
+	fmt.Printf("\nThe value of i is: %v\nthe address is: %v", i, &i)
 
 	*p = 10
 	fmt.Printf("\nThe value of p points to is: %v\nthe address is: %v", *p, p)
@@ -25,24 +25,27 @@ func main() {
 	var slice = []int{1, 2, 3}
 	var sliceCopy = slice
 	sliceCopy[2] = 4
-	fmt.Println(slice)
-	fmt.Println(sliceCopy)
+	fmt.Println(slice, &slice[0])         // prints [1 2 4] 0xc0000140b0
+	fmt.Println(sliceCopy, &sliceCopy[0]) // prints [1 2 4] 0xc0000140b0
 
 	var thing1 = [5]float64{1, 2, 3, 4, 5}
 	fmt.Printf("\nThe memory location of the thing1 array is %p", &thing1)
 	var thing2 = square(thing1)
-	fmt.Printf("\nThe memory location of the thing2 array is %p", &thing2)
+	fmt.Printf("\nThe memory location of the thing2 array is %p\n", &thing2)
 
-	fmt.Printf("\nThe result is: %v", thing2)
+	fmt.Printf("\nThe original thing1 array is: %v", thing1)
+	fmt.Printf("\nThe result is: %v\n", thing2)
 
 	var thing3 = [5]float64{1, 2, 3, 4, 5}
 	fmt.Printf("\nThe memory location of the thing3 array is %p", &thing3)
 	var result = squarePtr(&thing3)
 	fmt.Printf("\nThe result is: %v", result)
+	fmt.Printf("\nThe thing3 is: %v", thing3)
 	fmt.Printf("\nThe memory location of the result array is %p\n", &result)
 
 }
 
+// Function that receives an array and returns a new array with squared values
 func square(thing2 [5]float64) [5]float64 {
 	fmt.Printf("\nThe memory location of the thing2 array is %p", &thing2)
 
@@ -52,6 +55,7 @@ func square(thing2 [5]float64) [5]float64 {
 	return thing2
 }
 
+// Function that receives a pointer to an array and modifies the original array
 func squarePtr(arr *[5]float64) [5]float64 {
 	fmt.Printf("\nThe memory location of the arr array is %p", arr)
 	for i := range arr {
